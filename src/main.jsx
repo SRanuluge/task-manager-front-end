085c060f69e316ee0d9bc9df3504fe13ef9c5859
Sanjeewa Ranuluge <sbandara725@gmail.com>
2023-10-22 11:45:47 +1300

    updated package json

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import {
   RouterProvider,
   Route,
   createBrowserRouter,
-  createHashRouter,
 } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 import "../public/css/tailwind.css";
@@ -19,7 +18,7 @@ import { persist, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<App />}>
       <Route
