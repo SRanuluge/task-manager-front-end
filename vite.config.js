da5317e4c44c94f286b63b9fbab1de8fa39fb36c
Sanjeewa Ranuluge <sbandara725@gmail.com>
2023-10-22 23:44:04 +1300

    updated config file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  return {
+  const config = {
     plugins: [react()],
     resolve: {
       alias: [{ find: "@", replacement: "/src" }],
@@ -13,8 +13,13 @@ export default defineConfig(({ mode }) => {
         env.REACT_PUBLIC_API_BASE_URL
       ),
     },
-    base: "/task-manager-front-end/",
+    base: "/",
   };
+  if (command !== "serve") {
+    config.base = "/task-manager-front-end/";
+  }
+
+  return config;
 });
 
 // export default defineConfig(({ mode }) => {
