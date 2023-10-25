567e18ae95c80b84785ff258ceb9a74d762b3e66
Sanjeewa Ranuluge <sranuluge.dev@gmail.com>
2023-10-25 16:25:08 +1300

    fixed: image issue and remove env file

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -12,7 +12,7 @@ export function Profile() {
   return (
     <>
       <section className="relative block h-[50vh]">
-        <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('background-1.jpg')] bg-cover bg-center" />
+        <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/background-1.jpg')] bg-cover bg-center" />
         <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
       </section>
       <section className="relative bg-blue-gray-50/50 px-4 py-16">
